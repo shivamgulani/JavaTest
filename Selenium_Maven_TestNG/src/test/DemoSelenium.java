a5f3a1f162b999565d0f9be445bf9224b7e58ad3
unknown <shivamgulani@gmail.com>
2020-08-05 23:45:21 -0700

    hey

---
+++ Selenium_Maven_TestNG/src/test/DemoSelenium.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoSelenium {
+	
+	@Test
+	public void set_Up()
+	{
+		String driver_location="F:\\development\\jars\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driver_location);
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+	}
+	
+}
